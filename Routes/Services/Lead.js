05633628cbaebe6a084ffebc806797248dcad5f4
Suryansh Pandey <suryanshpsurya@gmail.com>
2024-03-26 17:15:36 +0530

    create lead route page

--- Routes/Services/Lead.js
+++ Routes/Services/Lead.js
@@ -15,5 +15,27 @@ router.post("/leads", async (req, res) => {
     }
 })
 
+router.get("/leads/:userId/:workspace", async (req, res) => {
+    const { userId, workspace } = req.params;
+
+    try {
+        const result = await Leads.find({ createdBy: new ObjectId(userId) })
+        res.status(200).send(result)
+    } catch (err) {
+        res.status(500).send({ error: err, message: "Internal server error" })
+    }
+})
+
+router.get("/lead/:leadId", async (req, res) => {
+    const { leadId } = req.params;
+
+    try {
+        const result = await Leads.findById(leadId)
+        res.status(200).send(result)
+    } catch (err) {
+        res.status(500).send({ error: err, message: "Internal server error" })
+    }
+})
+
 
 module.exports = router;
